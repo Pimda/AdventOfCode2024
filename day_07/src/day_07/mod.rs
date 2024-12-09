41cc6b8106beb86f3f12db25a17f24de9fe2e040
Pimda <2835860+Pimda@users.noreply.github.com>
2024-12-09 13:51:09 +0100

    Made day 7 faster

--- day_07/src/day_07/mod.rs
+++ day_07/src/day_07/mod.rs
@@ -27,7 +27,11 @@ impl Day<Vec<(u64, Vec<u64>)>, u64, u64> for Impl {
     fn part_1(&self, lines: &Vec<(u64, Vec<u64>)>) -> u64 {
         lines
             .iter()
-            .filter(|line| is_valid_1(line.0, 0, 0, &line.1))
+            .filter(|line| {
+                // load the first num before going into recursion
+                let num = get_num(&line.1, 0);
+                is_valid_1(line.0, num, 1, &line.1)
+            })
             .map(|v| v.0)
             .sum()
     }
@@ -35,7 +39,11 @@ impl Day<Vec<(u64, Vec<u64>)>, u64, u64> for Impl {
     fn part_2(&self, lines: &Vec<(u64, Vec<u64>)>) -> u64 {
         lines
             .iter()
-            .filter(|line| is_valid_2(line.0, 0, 0, &line.1))
+            .filter(|line| {
+                // load the first num before going into recursion
+                let num = get_num(&line.1, 0);
+                is_valid_2(line.0, num, 1, &line.1)
+            })
             .map(|v| v.0)
             .sum()
     }
@@ -46,14 +54,10 @@ fn is_valid_1(result: u64, accumulator: u64, index: usize, numbers: &[u64]) -> b
         return accumulator == result;
     }
 
-    let num = numbers.iter().nth(index).unwrap().to_owned();
+    let num = get_num(numbers, index);
 
-    if index == 0 {
-        is_valid_1(result, num, index + 1, numbers)
-    } else {
-        is_valid_1(result, num + accumulator, index + 1, numbers)
-            || is_valid_1(result, num * accumulator, index + 1, numbers)
-    }
+    is_valid_1(result, num + accumulator, index + 1, numbers)
+        || is_valid_1(result, num * accumulator, index + 1, numbers)
 }
 
 fn is_valid_2(result: u64, accumulator: u64, index: usize, numbers: &[u64]) -> bool {
@@ -61,18 +65,29 @@ fn is_valid_2(result: u64, accumulator: u64, index: usize, numbers: &[u64]) -> b
         return accumulator == result;
     }
 
-    let num = numbers.iter().nth(index).unwrap().to_owned();
+    let num = get_num(numbers, index);
 
-    if index == 0 {
-        is_valid_2(result, num, index + 1, numbers)
-    } else {
-        is_valid_2(result, num + accumulator, index + 1, numbers)
-            || is_valid_2(result, num * accumulator, index + 1, numbers)
-            || is_valid_2(
-                result,
-                format!("{}{}", accumulator, num).parse().unwrap(),
-                index + 1,
-                numbers,
-            )
+    is_valid_2(result, num + accumulator, index + 1, numbers)
+        || is_valid_2(result, num * accumulator, index + 1, numbers)
+        || is_valid_2(
+            result,
+            concat_numbers(accumulator, num),
+            index + 1,
+            numbers,
+        )
+}
+
+// required because somehow adding two nums as strings and parsing it again is very slow
+fn concat_numbers(accumulator: u64, num: u64) -> u64 {
+    let mut mul = 10;
+    
+    while mul <= num{
+        mul *= 10;
     }
+
+    accumulator * mul + num
+}
+
+fn get_num(numbers: &[u64], index: usize) -> u64 {
+    numbers.iter().nth(index).unwrap().to_owned()
 }
