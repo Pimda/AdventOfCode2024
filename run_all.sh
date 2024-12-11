c0f6a01820c4979f3acbdc6d2aa3f1a21545ed1f
Pimda <2835860+Pimda@users.noreply.github.com>
2024-12-11 16:11:44 +0100

    Changed run_all to include macxecutables

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Make sure we have an up-to-date release build
+# Make sure we have an up-to-date release build without older builds
+cargo clean
 cargo build --release
 
 # Set the target directory
 target_dir="target/release"
 
-# Loop through all files named day_*.exe in the target/release directory
-for exe in "$target_dir"/day_*.exe; do
+# Loop through all files named day_* in the target/release directory
+for exe in "$target_dir"/day_*; do
     # Check if the file exists and is executable
     if [ -f "$exe" ] && [ -x "$exe" ]; then
         echo "---$exe---"
