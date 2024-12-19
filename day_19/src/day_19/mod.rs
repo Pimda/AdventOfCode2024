686b38bb08539c4d352d9bea39f15b60505133f1
Pim <2835860+Pimda@users.noreply.github.com>
2024-12-19 21:38:33 +0100

    Made day 19 faster by putting the parts into a hashmap

--- day_19/src/day_19/mod.rs
+++ day_19/src/day_19/mod.rs
@@ -1,4 +1,4 @@
-use std::string::ToString;
+use std::{collections::HashMap, iter::Skip, str, string::ToString};
 
 use aoc_helper::{collections::MemoizerCollection, Day};
 
@@ -7,64 +7,81 @@ mod test;
 
 pub(crate) struct Impl {}
 
-impl Day<(Vec<String>, Vec<String>), usize, usize> for Impl {
-    fn parse(&self, input: String) -> (Vec<String>, Vec<String>) {
+impl Day<(HashMap<char, Vec<String>>, Vec<String>), usize, usize> for Impl {
+    fn parse(&self, input: String) -> (HashMap<char, Vec<String>>, Vec<String>) {
         let mut lines = input.lines();
 
-        let parts = lines
+        let parts: Vec<String> = lines
             .next()
             .unwrap()
             .split(", ")
             .map(ToString::to_string)
             .collect();
 
+        let mut map: HashMap<char, Vec<String>> = HashMap::new();
+
+        for part in parts {
+            let char = part.chars().next().unwrap();
+
+            if let Some(vec) = map.get_mut(&char) {
+                vec.push(part);
+            } else {
+                map.insert(char, vec![part]);
+            }
+        }
+
         lines.next();
-        (parts, lines.map(ToString::to_string).collect())
+        (map, lines.map(ToString::to_string).collect())
     }
 
-    fn part_1(&self, (parts, lines): &(Vec<String>, Vec<String>)) -> usize {
+    fn part_1(&self, (parts, lines): &(HashMap<char, Vec<String>>, Vec<String>)) -> usize {
         lines
             .iter()
-            .filter(|line| can_match(line, line.len(), 0, parts))
+            .filter(|line| can_match(line, 0, parts))
             .count()
     }
 
-    fn part_2(&self, (parts, lines): &(Vec<String>, Vec<String>)) -> usize {
+    fn part_2(&self, (parts, lines): &(HashMap<char, Vec<String>>, Vec<String>)) -> usize {
         lines
             .iter()
             .map(|line| {
                 let mut memoizer = MemoizerCollection::new();
-                count_matches(line, line.len(), 0, parts, &mut memoizer)
+                count_matches(line, 0, parts, &mut memoizer)
             })
             .sum()
     }
 }
 
-fn can_match(line: &String, line_len: usize, matched_len: usize, parts: &Vec<String>) -> bool {
-    if line_len == matched_len {
+fn can_match(line: &String, matched_len: usize, parts: &HashMap<char, Vec<String>>) -> bool {
+    if line.len() == matched_len {
         return true;
     }
 
-    parts.iter().any(|part| {
-        let line_chars = line.chars().skip(matched_len);
-        let part_chars = part.chars();
+    let char = line.chars().nth(matched_len).unwrap();
 
-        if iters_match(line_chars, part_chars) {
-            can_match(line, line_len, matched_len + part.len(), parts)
-        } else {
-            false
-        }
-    })
+    if let Some(possible_parts) = parts.get(&char) {
+        possible_parts.iter().any(|part| {
+            let line_chars = line.chars().skip(matched_len);
+            let part_chars = part.chars();
+
+            if iters_match(line_chars, part_chars) {
+                can_match(line, matched_len + part.len(), parts)
+            } else {
+                false
+            }
+        })
+    } else {
+        false
+    }
 }
 
 fn count_matches(
     line: &String,
-    line_len: usize,
     matched_len: usize,
-    parts: &[String],
+    parts: &HashMap<char, Vec<String>>,
     memoizer: &mut MemoizerCollection<usize, usize>,
 ) -> usize {
-    if line_len == matched_len {
+    if line.len() == matched_len {
         return 1;
     }
 
@@ -72,27 +89,29 @@ fn count_matches(
         return *count;
     }
 
-    let count = parts
-        .iter()
-        .map(|part| {
-            let line_chars = line.chars().skip(matched_len);
-            let part_chars = part.chars();
+    let char = line.chars().nth(matched_len).unwrap();
+    if let Some(possible_parts) = parts.get(&char) {
+        let count = possible_parts
+            .iter()
+            .map(|part| {
+                let line_chars = line.chars().skip(matched_len);
+                let part_chars = part.chars();
 
-            if iters_match(line_chars, part_chars) {
-                count_matches(line, line_len, matched_len + part.len(), parts, memoizer)
-            } else {
-                0
-            }
-        })
-        .sum();
+                if iters_match(line_chars, part_chars) {
+                    count_matches(line, matched_len + part.len(), parts, memoizer)
+                } else {
+                    0
+                }
+            })
+            .sum();
 
-    memoizer.add_and_return(matched_len, count)
+        memoizer.add_and_return(matched_len, count)
+    } else {
+        0
+    }
 }
 
-fn iters_match(
-    mut line_chars: std::iter::Skip<std::str::Chars<'_>>,
-    mut part_chars: std::str::Chars<'_>,
-) -> bool {
+fn iters_match(mut line_chars: Skip<str::Chars<'_>>, mut part_chars: str::Chars<'_>) -> bool {
     loop {
         let line_char = line_chars.next();
         let part_char = part_chars.next();
