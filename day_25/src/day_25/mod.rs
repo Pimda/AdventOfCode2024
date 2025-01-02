3c00213d17c1bd30425f685726eeab214b3f5236
Pimda <2835860+Pimda@users.noreply.github.com>
2025-01-02 16:21:56 +0100

    Implemented day 25

--- day_25/src/day_25/mod.rs
+++ day_25/src/day_25/mod.rs
@@ -5,16 +5,74 @@ mod test;
 
 pub(crate) struct Impl {}
 
-impl Day<Vec<Vec<char>>, usize, usize> for Impl {
-    fn parse(&self, input: String) -> Vec<Vec<char>> {
-        input.lines().map(|line| line.chars().collect()).collect()
+impl Day<(Vec<Vec<i32>>, Vec<Vec<i32>>), usize, ()> for Impl {
+    fn parse(&self, input: String) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
+        let mut keys = Vec::new();
+        let mut locks = Vec::new();
+
+        let mut buffer: Vec<Vec<char>> = Vec::new();
+
+        for line in input.lines() {
+            if line.is_empty() {
+                parse_locks_or_keys(&mut keys, &mut locks, &buffer);
+
+                buffer = Vec::new();
+            } else {
+                buffer.push(line.chars().collect());
+            }
+        }
+
+        parse_locks_or_keys(&mut keys, &mut locks, &buffer);
+
+        (locks, keys)
     }
 
-    fn part_1(&self, board: &Vec<Vec<char>>) -> usize {
-        todo!()
+    fn part_1(&self, (locks, keys): &(Vec<Vec<i32>>, Vec<Vec<i32>>)) -> usize {
+        let mut count = 0;
+
+        for lock in locks {
+            for key in keys {
+                if lock
+                    .iter()
+                    .zip(key.iter())
+                    .all(|(lock, key)| lock + key <= 7)
+                {
+                    println!("{lock:?}, {key:?} fit");
+
+                    count += 1;
+                } else {
+                    println!("{lock:?}, {key:?} overlap");
+                }
+            }
+        }
+
+        count
     }
 
-    fn part_2(&self, board: &Vec<Vec<char>>) -> usize {
-        todo!()
+    fn part_2(&self, (_, _): &(Vec<Vec<i32>>, Vec<Vec<i32>>)) -> () {}
+}
+
+fn parse_locks_or_keys(
+    keys: &mut Vec<Vec<i32>>,
+    locks: &mut Vec<Vec<i32>>,
+    buffer: &Vec<Vec<char>>,
+) {
+    let mut column_lengths = Vec::new();
+
+    for x in 0..5 {
+        let mut column_length = 0;
+        for y in 0..7 {
+            let char = buffer[y][x];
+            if char == '#' {
+                column_length += 1;
+            }
+        }
+        column_lengths.push(column_length);
+    }
+
+    if buffer[0].iter().all(|char| *char == '#') {
+        locks.push(column_lengths);
+    } else {
+        keys.push(column_lengths);
     }
 }
--- day_25/src/day_25/test.rs
+++ day_25/src/day_25/test.rs
@@ -3,20 +3,10 @@ use aoc_helper::runner::Runner;
 
 #[test]
 fn part_1_works_for_test() {
-    Runner::from_test_file(&Impl {}).assert_part_1(1);
+    Runner::from_test_file(&Impl {}).assert_part_1(3);
 }
 
 #[test]
 fn part_1_works_for_input() {
-    Runner::from_input_file(&Impl {}).assert_part_1(1);
+    Runner::from_input_file(&Impl {}).assert_part_1(3320);
 }
-
-// #[test]
-// fn part_2_works_for_test() {
-//     Runner::from_test_file(&Impl {}).assert_part_2(0);
-// }
-
-// #[test]
-// fn part_2_works_for_input() {
-//     Runner::from_input_file(&Impl {}).assert_part_2(1027164);
-// }
